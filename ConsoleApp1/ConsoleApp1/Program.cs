5b40ec11a847175e466840edff129ba4feb5da57
s18986 <s18986@LAB220-19>
2020-03-05 11:11:55 +0100

    #kolejny kommit

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var client = new HttpClient();
+            var result = await client.GetAsync("https://www.pja.edu.pl");
+            //Threadpool() - zestaw wiecznie otwartych watkow
+            if (result.IsSuccessStatusCode) // 2xx
+            {
+                string html = await result.Content.ReadAsStringAsync();
+                Console.WriteLine(html);
+                var regex = new Regex("[a-z]+[a-z0-9]*@[a-z.]+");
+                var matches = regex.Matches(html);
+                foreach( var m in matches)
+                {
+                    Console.WriteLine(m);
+                }
+            }
+            Console.WriteLine("KONIEC!");
         }
     }
 }
