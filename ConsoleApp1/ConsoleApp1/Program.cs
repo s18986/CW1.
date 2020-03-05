d6b2c913a1eb9e1ec4198d1f17aacdfbff5bddd1
s18986 <s18986@LAB220-19>
2020-03-05 11:46:50 +0100

    Wiadomosc

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -9,19 +10,34 @@ namespace ConsoleApp1
     {
         public static async Task Main(string[] args)
         {
-            var client = new HttpClient();
-            var result = await client.GetAsync("https://www.pja.edu.pl");
-            //Threadpool() - zestaw wiecznie otwartych watkow
-            if (result.IsSuccessStatusCode) // 2xx
+            try
             {
-                string html = await result.Content.ReadAsStringAsync();
-                Console.WriteLine(html);
-                var regex = new Regex("[a-z]+[a-z0-9]*@[a-z.]+");
-                var matches = regex.Matches(html);
-                foreach( var m in matches)
+                if (args.Length == 0) throw new ArgumentException("Parametr URL nie zostal podany");
+
+                string url = args.Length > 0 ? args[0] : "https://www.pja.edu.pl";
+                var client = new HttpClient();
+                var result = await client.GetAsync(url);
+                // Kolekcje
+                var list = new List<string>();
+                var slownik = new Dictionary<string, int>();
+
+                //Threadpool() - zestaw wiecznie otwartych watkow
+                if (result.IsSuccessStatusCode) // 2xx
                 {
-                    Console.WriteLine(m);
+                    string html = await result.Content.ReadAsStringAsync();
+                    Console.WriteLine(html);
+                    var regex = new Regex("[a-z]+[a-z0-9]*@[a-z.]+");
+                    var matches = regex.Matches(html);
+                    foreach (var m in matches)
+                    {
+                        Console.WriteLine(m);
+                    }
                 }
+            }
+            catch(Exception exc)
+            {
+                string blad = string.Format("Wystapil blad {0}", exc.ToString());
+                Console.WriteLine("wystapil blad" + exc.ToString());
             }
             Console.WriteLine("KONIEC!");
         }
